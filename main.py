de5b5a954c304472d39dd56f96c86a49a3bfe670
Rolly Maulana Awangga <rolly@awang.ga>
2015-07-02 23:38:29 +0700

    add gmail support

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python2.6
 import Skype4Py
+from gmail import Gmail
 
 skype = Skype4Py.Skype()
 skype.Attach()
-skype.SendMessage('noviazhang01','hello there')
-print 'done send message'
+
+g = Gmail()
+g.login('sms@kitaklik.com','rollygantengsekali')
+unread = g.inbox().mail(unread=True, sender="awg@kitaklik.com")
+try:
+	unread[0].fetch()
+	nomor = unread[0].subject.split('+')[1]
+	pesan = unread[0].body.split('#')[0]
+	print nomor
+	print pesan
+	skype.SendMessage(pesan,'Thank for your contacting me')
+	unread[0].read()
+	print 'done send message'
+except IndexError:
+	print "no unread email"
+
