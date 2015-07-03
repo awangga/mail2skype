0a1160543260642b5500cca3ad5542783d37e34c
Rolly Maulana Awangga <rolly@awang.ga>
2015-07-03 10:43:59 +0700

    add loop

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python2.6
 import Skype4Py
 from gmail import Gmail
+import time
 
 skype = Skype4Py.Skype()
 skype.Attach()
 
-g = Gmail()
-#change to your gmail account and password
-g.login('yourgmail@gmail.com','yourpassword')
-
-unread = g.inbox().mail(unread=True)
-try:
-	unread[0].fetch()
-	nomor = unread[0].subject
-	pesan = unread[0].body.split('#')[0]
-	print nomor
-	print pesan
-	skype.SendMessage(pesan,'Thank for your contacting me')
-	unread[0].read()
-	print 'done send message'
-except IndexError:
-	print "no unread email"
+while True:
+	g = Gmail()
+	#change to your gmail account and password
+	g.login('yourgmail@gmail.com','yourpassword')
+	unread = g.inbox().mail(unread=True)
+	try:
+		unread[0].fetch()
+		nomor = unread[0].subject
+		pesan = unread[0].body.split('#')[0]
+		print nomor
+		print pesan
+		skype.SendMessage(pesan,'Thank for your contacting me')
+		unread[0].read()
+		print 'done send message'
+		time.sleep(10)
+	except IndexError:
+		print "no unread email"
+		time.sleep(10)
 
