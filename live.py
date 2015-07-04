5fdf851caef17f29404ff96c6360e385ebd57b16
Rolly Maulana Awangga <rolly@awang.ga>
2015-07-05 01:27:59 +0700

    live is now supprted outlook

--- live.py
+++ live.py
@@ -1,14 +1,59 @@
 import imaplib
-def login():
-    while True:
-		imap = imaplib.IMAP4_SSL('imap-mail.outlook.com')
-		r, d = imap.login('rollyawangga@outlook.com', 'Mambono5')
-		assert r == 'OK', 'login failed'
-		try:
-			print imap.list()
-			imap.select('Inbox')
-		except imap.abort, e:
-			print "not connected"
-			continue
-		imap.logout()
-		break
+import email
+
+class Live():
+	def __init__(self):
+		import imaplib
+		
+	def login(self):
+	    while True:
+			self.imap = imaplib.IMAP4_SSL('imap-mail.outlook.com')
+			r, d = self.imap.login('rollyawangga@outlook.com', 'Mambono5')
+			assert r == 'OK', 'login failed'
+			try:
+				print "Connected as ",d
+			except self.imap.abort, e:
+				print "not connected"
+				continue
+			#self.imap.logout()
+			break
+			
+	def list(self):
+		#self.login()
+		return self.imap.list()
+	
+	def select(self,str):
+		return self.imap.select(str)
+		
+	def inbox(self):
+		return self.imap.select("Inbox")
+	
+	def logout(self):
+		return self.imap.logout()
+		
+	def unread(self):
+		r, d = self.imap.search(None, "UNSEEN")
+		list = d[0].split(' ')
+		latest_id = list[-1]
+		r, d = self.imap.fetch(latest_id, "(RFC822)")
+		self.raw_email = d[0][1]
+		self.email_message = email.message_from_string(self.raw_email)
+		return self.email_message
+	
+	def mailbody(self):
+		if self.email_message.is_multipart():
+			for payload in self.email_message.get_payload():
+				# if payload.is_multipart(): ...
+				return payload.get_payload()
+		else:
+			return self.email_message.get_payload()
+
+	def mailsubject(self):
+		return self.email_message['Subject']		
+		
+	def mailfrom(self):
+		return self.email_message['from']
+		
+	def mailto(self):
+		return self.email_message['to']
+		
