71b83551cd1c6f51351ffc8d1780eeefc87a932d
Rolly Maulana Awangga <rolly@awang.ga>
2015-07-05 07:53:17 +0700

    add outlook account

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ def success():
 # this is just a variable
 email = 'skype@passionit.net'
 password = 'rollyganteng'
+outlook_email = 'rollyawangga@outlook.com'
+outlook_password = 'Mambono5'
 intromsg = 'Tell me more about your interest in '
 nomail = 'no unread email'
 noword = 'no skype word in email body'
