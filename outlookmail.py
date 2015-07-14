fb7d6042f1bb3b5675baaa1de402b7b471833781
Rolly Maulana Awangga <rolly@awang.ga>
2015-07-14 20:59:26 +0700

    set 3 sec

--- outlookmail.py
+++ outlookmail.py
@@ -45,10 +45,10 @@ def checkingFolder(folder):
 			print "  subject : "+subject
 			print "  content : "+config.replymessage
 			mail.sendEmail(mail.mailfrom().split('>')[0].split('<')[1],"Re : "+subject,config.replymessage)
-			time.sleep(30)
+			time.sleep(3)
 	except:
 		print config.noword
-		time.sleep(30)
+		time.sleep(3)
 
 
 while True:
